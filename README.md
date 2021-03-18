faec97ef1691b753bd884ae8adad29f0d0a44d48
Tomas Bellus <tomas.bellus@gmail.com>
2021-03-18 02:02:45 +0100

    add container NS retrieval script

--- README.md
+++ README.md
@@ -14,6 +14,43 @@
 - get all container names in the whole cluster
 - returns lines of format: `NODE_IP,CONTAINER_NAME`
 
+## get\_container\_ns.sh
+
+- get the inode number of the /proc/PID/ns/mnt file
+- it's effectively used for [BCC](https://github.com/iovisor/bcc) [execsnoop](https://github.com/iovisor/bcc/blob/master/tools/execsnoop.py) setup filtered with the container mount namespace
+
+exmaple usage for a sample `NODE_IP,CONTAINER_NAME` output from [get_al_containers.sh](./get_all_containers.sh):
+```bash
+NODE_IP=10.0.0.3
+CONTAINER_NAME=k8s_container_qwertyuiop
+echo "inode number: $(./get_container_ns.sh -a $NODE_IP -n $CONTAINER_NAME)"
+```
+
+example output:
+```
+inode number: 24819530
+```
+
+- the retrieved inode number should be added to the eBPF map as so:
+```bash
+bpftool map create /sys/fs/bpf/mnt_ns_set type hash key 8 value 4 entries 128 name mnt_ns_set flags 0
+
+INODE_NUMBER=24819530
+FILE=/sys/fs/bpf/mnt_ns_set
+if [ $(printf '\1' | od -dAn) -eq 1 ]; then
+ HOST_ENDIAN_CMD=tac
+else
+  HOST_ENDIAN_CMD=cat
+fi
+
+NS_ID_HEX="$(printf '%016x' $INODE_NUMBER | sed 's/.\{2\}/&\n/g' | $HOST_ENDIAN_CMD)"
+bpftool map update pinned $FILE key hex $NS_ID_HEX value hex 00 00 00 00 any
+```
+
+- finally run the execsnoop script to monitor the process in a single container:
+```bash
+execsnoop --mntnsmap /sys/fs/bpf/mnt_ns_set
+```
 
 # Author
 
---
+++ get_container_ns.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#
+# get the mount namespace of the specified container
+# return the process ns/mnt inode number i.e. using `stat -c '%i' /proc/$PID/ns/mnt`
+#
+
+# GLOBALS
+
+USAGE="
+USAGE: $0 -a NODE_HOST -n CONTAINER_NAME [-u SSH_USER] [-c (docker | ...)] [-v]
+"
+HELP="$USAGE
+\t\t-a,--node HOST\t\tk8s node reachable ip or host
+\t\t-c,--engine ENGINE\tspecify the container engine (defaults to 'docker')
+\t\t-n,--name CONTAINER\tname of the container to inspect
+\t\t-u,--ssh-user USER\tk8s node ssh user (defaults to 'vagrant')
+\t\t-v\t\t\tverbose setting
+\t\t-h,--help\t\tprints this help
+"
+
+declare -A REQUIRED_ARGS
+REQUIRED_ARGS[-a]=1
+REQUIRED_ARGS[-n]=1
+
+VERBOSE=0  # verbose logging
+
+fail() {
+    echo -e "error: $*"
+    echo -e "   $USAGE"
+    exit 1
+}
+
+warn() {
+    echo -e "warning: $*"
+}
+
+debug() {
+    [[ $VERBOSE -eq 1 ]] && echo -e "DEBUG: $*"
+}
+
+# OPTION VARIABLES
+
+container_engine="docker"
+ssh_user="vagrant"
+
+# TODO # change the ARGUMENTS LOOP
+####################
+## ARGUMENTS LOOP ##
+####################
+parse_args() {
+    argvs=($@)
+
+    while (( "$#" )); do
+        case "$1" in
+            -a|--node)
+                [[ ${REQUIRED_ARGS[-a]} -eq 1 ]] && REQUIRED_ARGS[-a]=0
+                [[ $# -ge 2 ]] || fail "'-a': missing required parameter"
+                k8s_node_host="$2"
+                shift
+                ;;
+            -n|--name)
+                [[ ${REQUIRED_ARGS[-n]} -eq 1 ]] && REQUIRED_ARGS[-n]=0
+                [[ $# -ge 2 ]] || fail "'-n': missing required parameter"
+                container_name="$2"
+                shift
+                ;;
+            -c|--engine)
+                [[ ${REQUIRED_ARGS[-c]} -eq 1 ]] && REQUIRED_ARGS[-c]=0
+                [[ $# -ge 2 ]] || fail "'-c': missing required parameter"
+                shift
+
+                case "$1" in
+                    docker)
+                        ;;
+                    *)
+                        [[ $# -gt 0 ]] && fail "'-c': unknown container engine '$1'"
+                        ;;
+                esac
+                container_engine="$1"
+                ;;
+            -u|--ssh-user)
+                [[ ${REQUIRED_ARGS[-u]} -eq 1 ]] && REQUIRED_ARGS[-u]=0
+                [[ $# -ge 2 ]] || fail "'-u': missing required parameter"
+
+                shift
+                ;;
+            -v|--verbose)
+                VERBOSE=1
+                ;;
+            --help|-h)
+                echo -e "$HELP" && exit 0
+                ;;
+            *)
+                [[ $# -gt 0 ]] && fail "unknown argument '$1'"
+                ;;
+        esac
+        shift
+    done
+    debug "parsed all arguemnts"
+}
+
+parse_args "$@"
+
+# check if all required arguments were passed
+for arg in "${!REQUIRED_ARGS[@]}"; do
+    [[ ${REQUIRED_ARGS[$arg]} -eq 1 ]] && fail "missing required argument '$arg'"
+done
+
+# debug "-a is '$k8s_node_host'"
+# debug "-n is '$container_name'"
+
+##########
+## MAIN ##
+##########
+
+NS=$(ssh $ssh_user@$k8s_node_host CONTAINER=$container_name 'bash -s' <<-"ENDSSH"
+    id=$(sudo docker inspect $CONTAINER | jq -r '.[0].Config.Labels."io.kubernetes.sandbox.id"');
+    ppid=$(ps wax | grep containerd | grep "$id" | cut -d' ' -f1);
+    pid=$(pgrep -P $ppid | head -1);
+    echo $(sudo stat -c '%i' /proc/$pid/ns/mnt)
+ENDSSH
+)
+
+echo $NS
+
+exit 0
